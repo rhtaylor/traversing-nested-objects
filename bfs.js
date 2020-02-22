b3234e517ebd8c51ad17a511031b92e6cf833daf
Randy <randyisbusy@gmail.com>
2020-02-22 14:35:56 -0700

    will increase ability to deal with nested datastructures

--- bfs.js
+++ bfs.js
@@ -2,45 +2,59 @@ function find(array, criteriaFn) {
     let current = array
     let next = []
     while (current || current === 0) {
-
-        if (criteriaFn(current)) {
+        
+        if (criteriaFn(current)) { 
+            
             return current
         }
-
-        if (Array.isArray(current)) {
+        
+        if (Array.isArray(current)) {  
             for (let i = 0; i < current.length; i++) {
+                if (criteriaFn(current[i])){ 
+                     return current[i]} 
+                else 
                 next.push(current[i])
             }
         }
         else if (typeof current === "object") {
-            for (key in current) {
-                if (criteriaFn(current[key])) {
-                    return current[key]
+            
+            for (key in current) { 
+                if (criteriaFn(current[key])){
+                                   
+                    return current[key] }
 
-                    if (Array.isArray(current[key])) {
-                        for (let i = 0; i < current[key]; i++) {
-                            next.push(current[i])
-                        }
+                    if (Array.isArray(current[key])) { 
+                        
+                        for (let i = 0; i < current[key].length; i++) { 
+                            if (criteriaFn(current[key][i]) ){
+                                return current[key][i]
+                            } else  next.push(current[key][i]) 
+                            }
                     }
                     else if (typeof current[key] === "object") {
-                        for (keyz in current[key]) {
-                            next.push(current[keyz])
-                        }
-                    }
-                }
+                        for (keyz in current[key]) 
+                            if (criteriaFn(current[key][keyz])){ return current[key][keyz] } 
+                            else 
+                            next.push(current[key][keyz])
+                        }  
+                    
+                    }  
+                
+                    current = next.shift()
+                } 
+              current = next.shift()
             }
-
+            return null
+               
         }
+        
+       
+    
 
-        current = next.shift()
-    }
+let me = { object: { subObj: 1}, dataStructure: [ {triplearray: [100000000]} ] }
+    
 
+console.log("Hmm")
+let xyz = find(me, num => (num > 4)) 
 
-    return null
-
-}
-
-const numberCollections = [1, [2, { noPass: 4, pass: 12 }]]
-
-let x = find(numberCollections, number => (typeof number === 'number' && number > 5))
-console.log(x)
+console.log(xyz)
