4edc5c40e78d99d4ed8a1813f835207a24e45165
Randy <randyisbusy@gmail.com>
2020-02-22 23:30:13 -0700

    better handling of nested objects and arrays

--- bfs.js
+++ bfs.js
@@ -9,7 +9,8 @@ function find(array, criteriaFn) {
         }
         
         if (Array.isArray(current)) {  
-            for (let i = 0; i < current.length; i++) {
+            for (let i = 0; i < current.length; i++) { 
+                debugger
                 if (criteriaFn(current[i])){ 
                      return current[i]} 
                 else 
@@ -18,14 +19,16 @@ function find(array, criteriaFn) {
         }
         else if (typeof current === "object") {
             
-            for (key in current) { 
+            for (key in current) {  
+                
                 if (criteriaFn(current[key])){
                                    
                     return current[key] }
 
                     if (Array.isArray(current[key])) { 
                         
-                        for (let i = 0; i < current[key].length; i++) { 
+                        for (let i = 0; i < current[key].length; i++) {  
+                            debugger
                             if (criteriaFn(current[key][i]) ){
                                 return current[key][i]
                             } else  next.push(current[key][i]) 
@@ -33,7 +36,7 @@ function find(array, criteriaFn) {
                     }
                     else if (typeof current[key] === "object") {
                         for (keyz in current[key]) 
-                            if (criteriaFn(current[key][keyz])){ return current[key][keyz] } 
+                            if (criteriaFn(current[key][keyz]) ){ return current[key][keyz] } 
                             else 
                             next.push(current[key][keyz])
                         }  
@@ -47,14 +50,15 @@ function find(array, criteriaFn) {
             return null
                
         }
-        
-       
-    
-
-let me = { object: { subObj: 1}, dataStructure: [ {triplearray: [100000000]} ] }
-    
-
+// tests 
+// let arrayText = [23, 46, 2, 3, [2,1] ]  
+// let a =  funArrayTest = find(arrayText, num => (num < 2))
+// console.log(a) 
+// let objTest = { arrayKey: [0,2,3,4], objKey: { imNested: [2,3,4,5]}} 
+// let b = find(objTest, num => (num === 5)) 
+// console.log(b)
+let me = { object: { subObj: 1}, dataStructure: [ { triplearray: [100000000] } ] }
 console.log("Hmm")
-let xyz = find(me, num => (num > 4)) 
+let xyz = find(me, num => (typeof num === 'number' && num > 4)) 
 
 console.log(xyz)
